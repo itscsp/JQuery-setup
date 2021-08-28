e2d296eb637e0d2d86f77e188e859e15d51c1560
itscsp <cspoojary7972@gmail.com>
2021-08-28 19:43:53 +0530

    Initial commit on events

--- js/main.js
+++ js/main.js
@@ -1,33 +1,18 @@
 $(document).ready(function(){
-   // $('h1').hide();
-   // $('h1#heading1').hide();
-    //$('h1.heading2').hide();
-    $('p span').css('color','red');
-
-    $('ul#list li').css('list-style','none')
-    $('#list li:first').css("color",'red')
-    $('#list li:nth(2)').css("color",'brown')
-
-
-    //odd or even selector
-    // mouse hover over function
-    $('#list li').hover(function(){
-        
-        $(this).css({"backgroundColor":"blue","color":"white"})
-    },
-    function() {
-        $(this).css({"backgroundColor":"#f4f4f4","color":"black"})
-    }
-    )
-
-    // $(':button').fadeOut();
-    $(':submit').fadeIn();
-
-    //attribute selectors
-    $('[href]').css({'color':'red' });
-    $('[href="http://yahoo.com/"]').css({'color':'green' });
-
-    // #COMPELETE SELECTOR
-    // $('*').hide();
-
+   //EVENTS IN JQUERY
+   /*
+   $('#btn1').click(function(){
+	   alert('Button 1 clicked')
+   });
+    
+   $("#btn2").on('click', function(){
+	   
+	   alert('Button 2 clicked')
+   });
+   */
+   
+   $('#btn1').on('click', function()  {
+	  $('.para').innerText().css('color':'red');
+   });
+   
 });
